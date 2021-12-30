b0f7f350a2c1eef0d57c68398f93bf4120a1ea7d
mullane-ian <mullane.ian@outlook.com>
2021-12-29 17:34:27 -0700

    removed useEffect from loop

--- app.js
+++ app.js
@@ -1,37 +1,70 @@
-const express = require('express')
+const fs = require("fs");
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
 
-const app = express()
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-var cors = require('cors')
+const getdata = async () => {
+  try {
+    const datae = await fetch(
+      "https://testnets-api.opensea.io/assets?asset_contract_address=0x00529bf7DC6d4E831272632b3db5723406cb68E3&order_direction=asc",
+      {
+        method: "GET",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+    const data = await datae.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
 
-app.use(express.json())
-app.use(cors())
+app.get("/", async (req, res) => {
+  const mainreturn = await getdata();
+  console.log("user hit the resource");
 
-const fetch = (...args) => import('node-fetch').then(({default:fetch}) => fetch(...args))
+  res.status(200).json(mainreturn);
+});
+let mesh = [];
 
-const getdata=async()=>{
-    try {
-        const datae = await fetch('https://testnets-api.opensea.io/assets?asset_contract_address=0x00529bf7DC6d4E831272632b3db5723406cb68E3&order_direction=asc',{
-            method: 'GET',
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        })
-        const data = await datae.json()
-        console.log(data)
-        return data
-    } catch(err){
-        return err
+// fs.readFile("./count.txt", "utf8", function (err, data) {
+//   let array = JSON.parse("[" + data + "]");
+//   res.send(array);
+// });
+// fs.writeFile("count.txt", req.body.data.toString(), (err) => {
+//   if (err) console.log(err);
+// });
+app.post("/meshitemclicked", (req, res) => {
+  console.log("post called");
+  fs.writeFile("count.txt", req.body.data.toString(), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("written");
     }
-}
+  });
+});
+app.get("/meshitemclicked", (req, res) => {
+  fs.readFile("./count.txt", "utf8", function (err, data) {
+    let array = JSON.parse("[" + data + "]");
+    console.log("get called");
+    res.send(array);
+  });
+});
 
-
-app.get('/',async(req, res) => {
-    const mainreturn = await getdata()
-    console.log('user hit the resource')
-
-    res.status(200).json(mainreturn)
-})
-
-const port = process.env.PORT || 30002
-app.listen(port) 
+const port = process.env.PORT || 30002;
+app.listen(port, console.log("listening on " + port));
