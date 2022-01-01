3f395bac2c8ebd67354f2e093fa1e5814e665d98
mullane-ian <mullane.ian@outlook.com>
2022-01-01 13:10:24 -0700

    mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
+// require("./db")();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-var cors = require("cors");
+let cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
@@ -10,6 +11,25 @@ app.use(
     optionSuccessStatus: 200,
   })
 );
+const db = process.env.DB;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`connected to mongoDB`))
+  .catch((err) => console.log("error connecting to mongoDB", err));
+
+const schema = new mongoose.Schema(
+  {
+    clicks: {
+      type: String,
+      required: true,
+    },
+  }
+  // ,{ timestamps: true, }
+);
+const Click = mongoose.model("Click", schema);
 
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
@@ -39,31 +59,34 @@ app.get("/", async (req, res) => {
 
   res.status(200).json(mainreturn);
 });
-let mesh = [];
 
-// fs.readFile("./count.txt", "utf8", function (err, data) {
-//   let array = JSON.parse("[" + data + "]");
-//   res.send(array);
-// });
-// fs.writeFile("count.txt", req.body.data.toString(), (err) => {
-//   if (err) console.log(err);
-// });
-app.post("/meshitemclicked", (req, res) => {
-  console.log("post called");
-  fs.writeFile("count.txt", req.body.data.toString(), (err) => {
-    if (err) {
+let newClicks =
+  "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+
+app.post("/meshitemclicked", async (req, res) => {
+  Click.findOneAndUpdate({ clicks: req.body.data.toString() })
+    .then((data) => res.send(data))
+    .catch(async (err) => {
       console.log(err);
-    } else {
-      res.send("written");
-    }
-  });
+    });
 });
-app.get("/meshitemclicked", (req, res) => {
-  fs.readFile("./count.txt", "utf8", function (err, data) {
-    let array = JSON.parse("[" + data + "]");
-    console.log("get called");
-    res.send(array);
-  });
+app.get("/meshitemclicked", async (req, res) => {
+  const clicks = await Click.find();
+  // res.send(clicks);
+  if (clicks.length === 0) {
+    let click = new Click({ clicks: newClicks });
+    click = await click.save();
+    res.send(click);
+    console.log(click);
+  } else {
+    let result;
+    try {
+      result = JSON.parse("[" + clicks[0].clicks + "]");
+    } catch (error) {
+      console.log(error);
+    }
+    res.send(result);
+  }
 });
 
 const port = process.env.PORT || 30002;
